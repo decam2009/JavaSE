270068e9b81433242238a8cbc49ada5e4bf84923
Boris Kaloshin <decam2009@gmail.com>
2016-12-22 22:30:49 +0500

    HW0 Only arrays

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,91 @@
+import com.sun.org.glassfish.external.statistics.RangeStatistic;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = 0;
 
-    void clear() {
-    }
+    void clear()
+      {
+        for (int i = 0; i <= index; i ++)
+          {
+            storage[i] = null;
+            index = 0;
+          }
+      }
 
-    void save(Resume r) {
-    }
+    void save(Resume r)
+      {
+        storage [index] = r;
+        index ++;
+      }
 
-    Resume get(String uuid) {
-        return null;
-    }
+    Resume get(String uuid)
+      {
+        Resume tmp = new Resume();
+        for (int i = 0; i <= index - 1; i ++)
+          {
+            if (storage[i].uuid.equals(uuid))
+              {
+                tmp = storage[i];
+              }
+          }
+          return tmp;
+      }
 
-    void delete(String uuid) {
-    }
+    void delete(String uuid)
+      {
+        for (int i = 0; i <= index - 1; i++)
+          {
+            if (storage[i].uuid.equals(uuid))
+              {
+                storage[i].uuid = "-";
+              }
+          }
+      }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+    Resume[] getAll()
+      {
+        Resume tmp [] = new Resume[index];
+        int indexTmp = 0;
+        int j = 0;
 
-    int size() {
-        return 0;
-    }
+
+        for (int i = 0; i <= index - 1; i++)
+          {
+            tmp [i] = storage [i];
+          }
+
+        for (int i = 0; i <= tmp.length - 1; i ++)
+          {
+            if (!tmp [i].uuid.equals("-"))
+              {
+                indexTmp ++;
+              }
+          }
+          Resume [] output = new Resume[indexTmp];
+          index = 0;
+          for (int i = 0; i <= tmp.length - 1; i ++)
+            {
+              if (!tmp[i].uuid.equals("-"))
+                {
+                  output[j++] = tmp [i];
+                  index ++;
+                }
+            }
+          return output;
+      }
+
+    int size()
+      {
+        return index;
+      }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -16,14 +16,16 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r3.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
+        System.out.println("Size: " + ARRAY_STORAGE.size());
         ARRAY_STORAGE.delete(r1.uuid);
         printAll();
+        System.out.println("Size: " + ARRAY_STORAGE.size());
         ARRAY_STORAGE.clear();
         printAll();
 
