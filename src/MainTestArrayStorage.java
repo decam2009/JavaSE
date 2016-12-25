532609ca5991c9c6ed7b83d798d81688b4d564c1
Boris Kaloshin <decam2009@gmail.com>
2016-12-25 15:14:42 +0500

    HW1

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -11,10 +11,17 @@ public class MainTestArrayStorage {
         r2.uuid = "uuid2";
         Resume r3 = new Resume();
         r3.uuid = "uuid3";
+        Resume r4 = new Resume();
+        r4.uuid = "uuid4";
+        Resume r5 = new Resume();
+        r5.uuid = "uuid5";
+
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r4);
+        ARRAY_STORAGE.save(r5);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r3.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
